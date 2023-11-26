00827107fd91c496ce70312e7667cb8fdfa26a52
zhews <118117386+zhews@users.noreply.github.com>
2023-11-26 20:22:54 +0100

    feat: create REST API

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/zhews/gogreeter/greeter"
+)
 
 func main() {
-	fmt.Println("gogreeter")
+	app := fiber.New()
+
+	app.Get("/", getGreeting)
+
+	const ADDRESS = ":8080"
+	app.Listen(ADDRESS)
+}
+
+func getGreeting(c *fiber.Ctx) error {
+	input := c.Query("name")
+	output := greeter.Greet(input)
+	return c.Status(http.StatusOK).SendString(output)
 }
