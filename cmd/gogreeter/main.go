9ee3cd3a1b7d9748af82529a5a47c5533abfe641
zhews <118117386+zhews@users.noreply.github.com>
2023-12-18 13:01:44 +0100

    feat: add auto instrumentation to gogreeter

--- cmd/gogreeter/main.go
+++ cmd/gogreeter/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 
+	"github.com/gofiber/contrib/otelfiber/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/zhews/gogreeter/greeter"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 )
 
 func main() {
+	ctx := context.Background()
+	tracerProvider := setupOpenTelemetryTracerProvider(ctx)
+	defer func() {
+		if err := tracerProvider.Shutdown(ctx); err != nil {
+			log.Fatal("could not shutdown tracer provider", err)
+		}
+
+	}()
+	otel.SetTracerProvider(tracerProvider)
+
 	app := fiber.New()
+
+	app.Use(otelfiber.Middleware())
 
 	app.Get("/", getGreeting)
 	app.Get("/metrics", adaptor.HTTPHandler(promhttp.Handler()))
@@ -24,6 +43,27 @@ func main() {
 
 func getGreeting(c *fiber.Ctx) error {
 	input := c.Query("name")
-	output := greeter.Greet(input)
+	output := greeter.Greet(c.UserContext(), input)
 	return c.Status(http.StatusOK).SendString(output)
+}
+
+func setupOpenTelemetryTracerProvider(ctx context.Context) *trace.TracerProvider {
+	resource, err := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("gogreeter"),
+		),
+	)
+	if err != nil {
+		log.Fatal("could not setup OpenTelemetry", err)
+	}
+	exporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithEndpoint("jaeger:4317"), otlptracegrpc.WithInsecure())
+	if err != nil {
+		log.Fatal("could not setup OpenTelemetry", err)
+	}
+	return trace.NewTracerProvider(
+		trace.WithResource(resource),
+		trace.WithBatcher(exporter),
+	)
 }
